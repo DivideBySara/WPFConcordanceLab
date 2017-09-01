3d46a5eb983bb3664846a72eac4f30ad24d182d5
dividebysara <s.jade@bellevuecollege.edu>
2017-09-01 09:28:18 -0700

    Initial commit of mostly starter code + command line arguments + add WPF project

---
+++ Lab0831/Concordance/Analyzer.cs
@@ -0,0 +1,89 @@
+﻿using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace Lab0830
+{
+    internal struct Wordref
+    {
+        internal string word;
+        internal int paragraphIndex;
+        internal int sentenceIndex;
+    }
+
+    internal struct Paragraph
+    {
+        internal string text;
+        internal List<Sentence> sentences;
+
+    }
+
+    internal struct Sentence
+    {
+        internal string text;
+        internal List<Wordref> words;
+    }
+
+    public class Analyzer
+    {
+        List<Paragraph> paragraphs = new List<Paragraph>();
+
+        public void Analyze(string input)
+        {
+            SplitParagraphs(input);
+        }
+
+        // Find the paragraphs in the input
+        void SplitParagraphs(string @in)
+        {
+            string[] paragraphStrings;
+
+            Regex reg = new Regex("\r\n");
+            paragraphStrings = reg.Split(@in);
+            foreach (string pgstr in paragraphStrings)
+            {
+                if (pgstr != string.Empty)
+                {
+                    paragraphs.Add(new Paragraph { text = pgstr, sentences = new List<Sentence>() });
+                    SplitSentences(paragraphs[paragraphs.Count - 1], paragraphs.Count - 1);
+                }
+            }
+        }
+
+        // Find the sentences in a paragraph
+        void SplitSentences(Paragraph pg,int pgindex)
+        {
+            {
+                string[] sentenceStrings;
+
+                Regex reg = new Regex(@"[\.?!]");
+                sentenceStrings = reg.Split(pg.text);
+                foreach (string sent in sentenceStrings)
+                {
+                    pg.sentences.Add(new Sentence
+                    { text = sent, words = new List<Wordref>() });
+                    SplitWords(pg.sentences[pg.sentences.Count - 1],pgindex, pg.sentences.Count - 1);
+                }
+            }
+        }
+
+        // Find the words in each sentence
+        void SplitWords(Sentence sent, int pgindex, int sentindex)
+        {
+            string[] wordStrings;
+
+            Regex reg = new Regex(@"[\s+]");
+            Regex reg2 = new Regex(@"\W");
+            wordStrings = reg.Split(sent.text);
+            foreach (string wd in wordStrings)
+            {
+                sent.words.Add(new Wordref
+                {
+                    word = reg2.Replace(wd, "").ToLower(),
+                    paragraphIndex = pgindex,
+                    sentenceIndex = sentindex
+                });
+            }
+        }
+
+    }
+}
---
+++ Lab0831/Concordance/Concordance.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Console;
+
+namespace Lab0830
+{
+    class Concordance
+    {
+        static string inPath = string.Empty;
+        static string outPath = string.Empty;
+        static string excludedWordsPath = string.Empty;
+        static string inputText = string.Empty;
+        static string outputText = string.Empty;
+        static string[] excludedWords;
+
+        static void Main(string[] args)
+        {
+
+            Analyzer anlz = new Analyzer();
+
+            // Set up paths from args
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Invalid number of path specifications");
+                return;
+            }
+            GetPaths(args);
+            // Get the input data
+            ReadInputs();
+            // Identify paragraphs and sentences
+            // Identify words and their location
+            anlz.Analyze(inputText);
+
+            ReadKey();
+        }
+
+        static void ReadInputs()
+        {
+            try
+            {
+                inputText = File.ReadAllText(inPath);
+                excludedWords = File.ReadAllLines(excludedWordsPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error reading file " + e.Message);
+            }
+        }
+
+        static void GetPaths(string[] args)
+        {
+            // Get the path to the input text file
+            try
+            {
+                inPath = Path.GetFullPath(args[0]);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine($"Invalid text input path {args[0]}");
+            }
+
+            // Get the path for the output file
+            try
+            {
+                outPath = Path.GetFullPath(args[1]);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine($"Invalid text output path {args[1]}");
+            }
+
+            // Get the path to the excluded words file
+            try
+            {
+                excludedWordsPath = Path.GetFullPath(args[2]);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine($"Invalid excluded words input path {args[1]}");
+            }
+        }
+
+    }
+}
